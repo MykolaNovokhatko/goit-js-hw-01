9718ed9037f439b46ea114e0935aa92247bd225b
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-01-12 21:58:35 +0000

    task1.2.3

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
 function makeTransaction(quantity, pricePerDroid) {
-  return `You ordered ${quantity} droids worth ${
-    quantity * pricePerDroid
-  } credits!`;
+  //const totalPrice = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+
+  //return totalPrice;
+
+  const totalPrice = `${quantity * pricePerDroid}`;
+
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function getElementWidth(content, padding, border) {
+  content = content.replace(",", ".");
+  padding = padding.replace(",", ".");
+  border = border.replace(",", ".");
+
+  content = Number.parseFloat(content);
+  padding = Number.parseFloat(padding);
+  border = Number.parseFloat(border);
+
+  const widthTotal = `${content + padding * 2 + border * 2}`;
+
+  return widthTotal;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
